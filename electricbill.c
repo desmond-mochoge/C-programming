6b72d286b098ab6fb56972380e959bcb8f33b637
desmond-mochoge <desmondmochoge@gmail.com>
2025-10-23 21:10:12 +0300

    Update electricbill.c

--- electricbill.c
+++ electricbill.c
@@ -4,22 +4,26 @@
 */
 #include<stdio.h>
 
-int calculate_electric_bill(int units);
+int calculate_electric_bill(int units);//FUNCTION DEFINITION
 
 int main(){
 float bill;	
 int units;
+	
 printf("Enter number of units used\n");
 scanf("%d",&units);
-bill=calculate_electric_bill(units);
+	
+bill=calculate_electric_bill(units);   //FUNCTION CALL
+	
 printf("Total Electric bill is KES%.2f",bill);
 return 0;	
 }
-int calculate_electric_bill(int units){
+int calculate_electric_bill(int units){    //FUNCTION DECLARATION
 	float totalbill=0.0;
 	if(units <=100){
 	totalbill=units*10;
-	}else if(units<=200){
+	}
+	else if(units<=200){
 	totalbill=(100*10)+((units-100)*15);		
 	}
 	else{
@@ -27,3 +31,4 @@ int calculate_electric_bill(int units){
 	}
 	return totalbill;
 }
+
