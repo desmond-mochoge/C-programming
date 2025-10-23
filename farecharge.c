c67c3683bb3713374075aa855959354d40d33cd3
desmond-mochoge <desmondmochoge@gmail.com>
2025-10-23 21:14:53 +0300

    Update farecharge.c

--- farecharge.c
+++ farecharge.c
@@ -1,17 +1,25 @@
+/*NAME:DESMOND MESA
+REG NO:PA106/G/28781/25
+DESCRIPTION:PROGRAM TO CALCULATE FARE
+*/
 #include<stdio.h>
-int calculateFare(int kilometer);
+int calculateFare(int kilometer); //FUNCTION DEFINITON
 
-int main(){
+int main(){  
 float totalfare;	
 float kilometer;
+	
 printf("Enter distance travelled:\n");
 scanf("%f",&kilometer);
-totalfare=calculateFare(kilometer); 
+	
+totalfare=calculateFare(kilometer); //FUNCTION CALL
+	
 printf("Your total fare is KES%.2f",totalfare);
 return 0;	
 }
-int calculateFare(int kilometer){
+int calculateFare(int kilometer){  //FUNCTION DECLARATION
 	float totalfare;
 	totalfare=kilometer*50;
 	return totalfare;
+
 }
