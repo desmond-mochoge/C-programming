7a7eb2657f7df46a7aa8254ac5e74914d0e3d091
desmond-mochoge <desmondmochoge@gmail.com>
2025-10-31 09:02:15 +0300

    Refactor revenue tracker code with comments

--- 1DArray.c
+++ 1DArray.c
@@ -3,15 +3,15 @@
   DESCRIPTION:Weekly revenue tracker
 */
 
-
+//Header file
 #include<stdio.h>
-
+//main function
 int main(){
 	int i;
 	float sum=0;
 	int revenue[7];
 	float average;
-	
+	//loop function
 	for(i=0;i<7;i++){
 		printf("Enter revenue for day %d:",i+1);
 		scanf("%d",&revenue[i]);
@@ -19,7 +19,7 @@ int main(){
 		sum =sum +revenue[i];
 	}
 	average=sum/7;
-	
+	//Display revenue updated
 	printf("Your total weekly revenue is %.2f\n", sum);
 	
 	printf("Your average daily revenue is %.2f\n",average);
@@ -27,4 +27,5 @@ int main(){
 	return 0;
 	
 	
+
 }
